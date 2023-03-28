5c781e31a2dff988e2ba34d0e1e513717ad97c29
m-njora <mmuthekijora@gmail.com>
2023-03-28 14:57:41 +0300

    seeded data

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -3,6 +3,5 @@ class Signup < ApplicationRecord
   belongs_to :activity
   belongs_to :facilitator
 
-  validates :time, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "seeding data"
+# Create activities
+Activity.create(
+  name: "Hiking",
+  description: "Enjoy a beautiful hike in the mountains.",
+  location: "Mountains",
+)
+
+Activity.create(
+  name: "Swimming",
+  description: "Cool off and have some fun in the pool.",
+  location: "Pool",
+)
+
+# Create facilitators
+Facilitator.create(
+  name: "John Doe",
+  email: "john.doe@example.com",
+)
+
+Facilitator.create(
+  name: "Jane Smith",
+  email: "jane.smith@example.com",
+)
+
+# Create users
+User.create(
+  name: "Alice",
+  age: 12,
+  email: "alice@example.com",
+)
+
+User.create(
+  name: "Bob",
+  age: 16,
+  email: "bob@example.com",
+)
+
+# Create signups
+Signup.create(
+  user_id: 1,
+  activity_id: 1,
+  facilitator_id: 1,
+  attended: true,
+)
+
+Signup.create(
+  user_id: 2,
+  activity_id: 2,
+  facilitator_id: 2,
+  attended: false,
+)
+puts "done seeding"
