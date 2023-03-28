fd8b9f98b4ea0a3a91bea2c33ae13112cf666dfd
m-njora <mmuthekijora@gmail.com>
2023-03-28 14:39:35 +0300

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :signups
     has_many :activities, through: :signups
   
+    validates :name, presence: true
+    validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 8, less_than_or_equal_to: 18 }
+  
 end
